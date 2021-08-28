f66629f38805035616a8997eddd04cf8a060da16
cndidofernades <candidofrnnds@gmail.com>
2021-08-28 02:11:21 +0100

    fix bug baseUrl

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import {parseCookies} from 'nookies'
 
 const api = axios.create({
-    baseURL: 'http://localhost:3000/api/'
+    baseURL: 'https://ijnm.vercel.app/api/'
 })
 
 if(parseCookies()['igreja.token']) api.defaults.headers['Authorization'] = `Bearer ${parseCookies()['igreja.token']}`
